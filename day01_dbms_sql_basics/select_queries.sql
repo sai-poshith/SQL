4d8067b32df0b6261aa803908c145d208e6677e6
sai poshith reddy aramati <253964105+sai-poshith@users.noreply.github.com>
2026-01-29 20:14:50 +0530

    syntax & working of select

---
+++ day01_dbms_sql_basics/select_queries.sql
@@ -0,0 +1,9 @@
+-- View all records
+SELECT * FROM students;
+
+-- Select specific columns
+SELECT name, branch FROM students;
+
+-- Filter records using WHERE clause
+SELECT * FROM students WHERE age > 20;
+
